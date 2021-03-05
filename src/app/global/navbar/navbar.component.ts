d533107f3b3ea72af2a3a4c0e84c9fc1256d1768
Jan <s1120303@student.hsleiden.nl>
2021-03-05 23:00:27 +0100

    Changing navbar on scroll

--- src/app/global/navbar/navbar.component.ts
+++ src/app/global/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:scroll', ['$event'])
+
+  onWindowScroll(e) {
+    let element = document.querySelector('.navbar');
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('light');
+    } else {
+      element.classList.remove('light');
+    }
   }
 
 }
