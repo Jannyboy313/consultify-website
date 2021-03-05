2b61323d2079c734922ed5b74806a4a2610d0852
Jan <s1120303@student.hsleiden.nl>
2021-03-05 16:32:59 +0100

    Generated component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { OurTeamComponent } from './our-team/our-team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HeaderComponent
+    HeaderComponent,
+    OurTeamComponent
   ],
   imports: [
     BrowserModule,
