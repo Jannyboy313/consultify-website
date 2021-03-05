debad0cbb35710e312f4c34851ab211ca27f7d8c
Jan <s1120303@student.hsleiden.nl>
2021-03-05 22:23:58 +0100

    Clean up and generated component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { NgxPageScrollCoreModule } from 'ngx-page-scroll-core';
 import { NgxPageScrollModule } from 'ngx-page-scroll';
 
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './global/navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
 import { OurTeamComponent } from './our-team/our-team.component';
-import { HeadingComponent } from './heading/heading.component';
-import { FooterComponent } from './footer/footer.component';
+import { HeadingComponent } from './global/heading/heading.component';
+import { FooterComponent } from './global/footer/footer.component';
 import { CompanyComponent } from './company/company.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { CompanyComponent } from './company/company.component';
     OurTeamComponent,
     HeadingComponent,
     FooterComponent,
-    CompanyComponent
+    CompanyComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
