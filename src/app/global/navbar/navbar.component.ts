30b191f812be9a7e3458c928997870ecf827b45f
Jan <s1120303@student.hsleiden.nl>
2021-03-08 14:18:53 +0100

    Removed comments

--- src/app/global/navbar/navbar.component.ts
+++ src/app/global/navbar/navbar.component.ts
@@ -8,10 +8,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class NavbarComponent implements OnInit {
   constructor(translate: TranslateService) {
-    // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('nl');
-
-     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('nl');
 }
 
