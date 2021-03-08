5a38f329cc5827a749e1a62e82aa598f55fff01d
Jan <s1120303@student.hsleiden.nl>
2021-03-08 13:18:04 +0100

    Added translate service

--- src/app/global/navbar/navbar.component.ts
+++ src/app/global/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,7 +7,13 @@ import { Component, OnInit, HostListener } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
-  constructor() { }
+  constructor(translate: TranslateService) {
+    // this language will be used as a fallback when a translation isn't found in the current language
+    translate.setDefaultLang('nl');
+
+     // the lang to use, if the lang isn't available, it will use the current loader to get them
+    translate.use('nl');
+}
 
   ngOnInit(): void {
   }
